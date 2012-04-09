7f66e385f6cd2575bc895e8e18b574a1df5be27b
SergeyChernyh <chsv87@mail.ru>
2012-04-09 22:53:35 +0400

    remove unused

--- metaprogramming/unspecified.h
+++
@@ -1,6 +0,0 @@
-#ifndef __METAPROGRAMMING_UNSPECIFIED__
-#define __METAPROGRAMMING_UNSPECIFIED__
-
-namespace robot { namespace metaprogramming_tools { struct unspecified {}; }}
-
-#endif //__METAPROGRAMMING_UNSPECIFIED__
