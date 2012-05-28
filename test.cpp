fd899b05e5a842ac94c10f88e29711a92a80b9af
SergeyChernyh <chsv87@mail.ru>
2012-05-28 04:26:28 +0400

    test update

--- test.cpp
+++ test.cpp
@@ -37,7 +37,16 @@ void send_cmd(int16_t p, Interface& i)
     using cmd = command<n, int16_t>;
     using msg = message<cmd>;
 
-    auto m = package<msg>(p, chck_sum_calc<cmd>(package<cmd>(p)));
+    msg x;
+
+    sequence_access::at_c<2>(sequence_access::at_key<message_body_key>(x)) = p;
+    sequence_access::at_key<chck_sum_key>(x) = chck_sum_calc<cmd>(package<cmd>(p));
+
+    auto m = package<msg>(x);//p, chck_sum_calc<cmd>(package<cmd>(p)));
+
+    robot::package_creation::serialization::serialize_tmp_wrapper<msg>::deserialize(m.get_data(), x);
+
+    std::cout << "---> " << sequence_access::at_c<2>(sequence_access::at_key<message_body_key>(x)) << std::endl;
 
     i.write((const char*)(m.get_data()), package_creation::size<msg>::value);
 }
@@ -54,7 +63,19 @@ void send_vec_cmd(int16_t p, Interface& i)
 
     auto cmd_ = package<cmd>(vec);
 
-    auto m = package<msg>(cmd_.data_size() + package_creation::size<chck_sum_t<cmd>>::value, vec, chck_sum_calc<cmd>(cmd_));
+    msg x;
+
+    sequence_access::at_key<arg_key>(sequence_access::at_key<message_body_key>(x)) = vec;
+    sequence_access::at_key<chck_sum_key>(x) = chck_sum_calc<cmd>(cmd_);
+    sequence_access::at_key<byte_count_key>(sequence_access::at_key<message_header_key>(x)) = cmd_.data_size() + package_creation::size<chck_sum_t<cmd>>::value;
+
+    auto m = package<msg>(x);//cmd_.data_size() + package_creation::size<chck_sum_t<cmd>>::value, vec, chck_sum_calc<cmd>(cmd_));
+
+    sequence_access::at_key<arg_key>(sequence_access::at_key<message_body_key>(x))[0] = 333;
+
+    robot::package_creation::serialization::serialize_tmp_wrapper<msg>::deserialize(m.get_data(), x);
+
+    std::cout << "========> " << sequence_access::at_c<2>(sequence_access::at_key<message_body_key>(x))[0] << std::endl;
 
     i.write((const char*)(m.get_data()), m.data_size()); //TODO
 }
@@ -65,7 +86,12 @@ int main(int argc, const char* argv[])
 
     tcp.open();
 
+    uint8_t buffer[0xff];
+
     send_cmd<0>(tcp);
+
+    tcp.read(buffer, 3);
+
     send_cmd<1>(tcp);
     send_cmd<2>(tcp);
 
@@ -73,8 +99,8 @@ int main(int argc, const char* argv[])
 
     send_cmd<4 , 1>(tcp);
 
-    //send_cmd<11, 1200>(tcp);
-    //send_cmd<11>(1200, tcp);
+    send_cmd<11, 1200>(tcp);
+    send_cmd<11>(1200, tcp);
     send_vec_cmd<11>(1200, tcp);
 
     while(true) {
